ae8096c922a0810fe8e13dc777b3daea1bfa9cd1
Bas Dijkstra <bas@ontestautomation.com>
2022-05-23 15:18:06 +0200

    Initial commit

---
+++ src/test/java/sql/SqlTest.java
@@ -0,0 +1,28 @@
+package sql;
+
+import org.junit.jupiter.api.Test;
+
+public class SqlTest {
+
+    // Retrieve the number of customers from the database
+    // Check that the total number of customers is equal to 2
+    @Test
+    public void getAllCustomers_checkNumber_shouldBeTwo() {
+
+    }
+
+    // Retrieve all accounts for customer Sarah from the database
+    // Check that the total number of accounts for her is 2
+    @Test
+    public void getAccountsForSarah_checkNumber_shouldBeTwo() {
+
+    }
+
+    // A test that checks that the total of all transactions is equal to 0 for a given account.
+    // Doing this for a single account is enough,
+    // and you’re allowed to use the account ID for that account in the query
+    @Test
+    public void retrieveTransactionsForAccount_checkTotalBalance_shouldBeZero() {
+
+    }
+}
